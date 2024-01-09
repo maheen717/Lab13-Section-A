78fafefa9892212008117199875f08ef2e3c283f
maheen717 <156076019+maheen717@users.noreply.github.com>
2024-01-09 15:49:32 +0500

    Add files via upload

---
+++ JUnitTesting.java
@@ -0,0 +1,27 @@
+package Lab11;
+
+public class JUnitTesting {
+
+	public int square (int x) {
+		return x*x;
+		}
+		public int countA(String word)
+		{
+		int count = 0;
+		for (int i=0; i <word.length();i++)
+		{
+		if(word.charAt(i)=='a' || word.charAt(i)=='A')
+		{
+		count++;
+		}
+		}
+		return count;
+		}
+		
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
